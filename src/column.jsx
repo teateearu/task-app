2800a0e76d4be5d301a010710fcc58862eedcc41
“Tea <teateearu@gmail.com>
2018-10-07 17:31:21 +0200

    Created columns and tasks

--- src/column.jsx
+++ src/column.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  width: 220px;
+
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h3`
   padding: 8px;
 `;
 const TaskList = styled.div`
   padding: 8px;
+  transition: background-color 0.2s ease;
+  background: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 export default class Column extends React.Component {
@@ -19,9 +28,20 @@ export default class Column extends React.Component {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <TaskList>
-          {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
-        </TaskList>
+        <Droppable droppableId={this.props.column.id}>
+          {(provided, snapshot) => (
+            <TaskList
+              innerRef={provided.innerRef}
+              {...provided.droppableProps}
+              isDraggingOver={snapshot.isDraggingOver}
+            >
+              {this.props.tasks.map((task, index) => (
+                <Task key={task.id} task={task} />
+              ))}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
       </Container>
     );
   }
--- src/initial-data.js
+++ src/initial-data.js
@@ -1,18 +1,47 @@
 const initialData = {
   tasks: {
-    'task-1': { id: 'task-1', content: 'Take out the garbage' },
-    'task-2': { id: 'task-2', content: 'Watch' },
-    'task-3': { id: 'task-3', content: 'Charge' },
-    'task-4': { id: 'task-4', content: 'Cook' },
+    'task-1': { id: 'task-1', content: 'Item title' },
+    'task-2': { id: 'task-2', content: 'Item title' },
+    'task-3': { id: 'task-3', content: 'Item title' },
+    'task-4': { id: 'task-4', content: 'Item title' },
+    'task-5': { id: 'task-5', content: 'Item title' },
+    'task-6': { id: 'task-6', content: 'Item title' },
+    'task-7': { id: 'task-7', content: 'Item title' },
+    'task-8': { id: 'task-8', content: 'Item title' },
   },
   columns: {
     'column-1': {
       id: 'column-1',
-      title: 'To do',
-      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+      title: 'Flowcharts',
+      taskIds: ['task-1'],
+    },
+    'column-2': {
+      id: 'column-2',
+      title: 'Wireframes',
+      taskIds: [],
+    },
+    'column-3': {
+      id: 'column-3',
+      title: 'Prototype',
+      taskIds: ['task-2', 'task-3'],
+    },
+    'column-4': {
+      id: 'column-4',
+      title: 'Development',
+      taskIds: ['task-4', 'task-5', 'task-6'],
+    },
+    'column-5': {
+      id: 'column-5',
+      title: 'Test',
+      taskIds: [],
+    },
+    'column-6': {
+      id: 'column-6',
+      title: 'Launch',
+      taskIds: ['task-7', 'task-8'],
     },
   },
-  columnOrder: ['column-1'],
+  columnOrder: ['column-1', 'column-2', 'column-3', 'column-4', 'column-5', 'column-6'],
 };
 
 export default initialData;
