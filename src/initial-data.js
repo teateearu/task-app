9dccb1b992786069c68b2c45fad4156545a27140
“Tea <teateearu@gmail.com>
2018-10-07 14:54:06 +0200

    initial commit

---
+++ src/initial-data.js
@@ -0,0 +1,18 @@
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Take out the garbage' },
+    'task-2': { id: 'task-2', content: 'Watch' },
+    'task-3': { id: 'task-3', content: 'Charge' },
+    'task-4': { id: 'task-4', content: 'Cook' },
+  },
+  columns: {
+    'column-1': {
+      id: 'column-1',
+      title: 'To do',
+      taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+    },
+  },
+  columnOrder: ['column-1'],
+};
+
+export default initialData;
